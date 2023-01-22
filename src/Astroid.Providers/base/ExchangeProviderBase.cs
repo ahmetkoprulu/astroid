579b711162037cc193e94d57b0eb107117bac229
ahmetkoprulu <aensarkoprulu@gmail.com>
2023-01-22 11:58:02 +0300

    Added mapping exchange properties in to exchange provider instance

--- src/Astroid.Providers/base/ExchangeProviderBase.cs
+++ src/Astroid.Providers/base/ExchangeProviderBase.cs
@@ -23,7 +23,64 @@ public abstract class ExchangeProviderBase : IDisposable
 
 	public virtual void Context()
 	{
+		var propertyValues = JsonConvert.DeserializeObject<List<ProviderPropertyValue>>(Exchange.PropertiesJson);
+		BindProperties(propertyValues);
+	}
 
+	public void BindProperties(List<ProviderPropertyValue> propertyValues)
+	{
+		if (propertyValues == null) throw new ArgumentException("There is no property to bind this provider.");
+
+		var provider = this;
+		var type = GetType();
+		var properties = type.GetProperties().Where(x => x.GetCustomAttribute<PropertyMetadataAttribute>() != null).ToList();
+
+		foreach (var property in properties)
+		{
+			var propertyValue = propertyValues.SingleOrDefault(p => p.Property == property.Name);
+			if (propertyValue?.Value == null) continue;
+
+			if (property.PropertyType == propertyValue.Value.GetType())
+			{
+				property.SetValue(provider, propertyValue.Value);
+				continue;
+			}
+
+			try
+			{
+				if (property.PropertyType.IsGenericType)
+				{
+					var propertyValueString = $"{propertyValue.Value}";
+					if (!string.IsNullOrWhiteSpace(propertyValueString))
+					{
+						property.SetValue(provider,
+							JsonConvert.DeserializeObject(propertyValueString, property.PropertyType));
+					}
+
+					continue;
+				}
+
+				var converter = TypeDescriptor.GetConverter(property.PropertyType);
+				var objValue = converter.ConvertFrom(propertyValue.Value.ToString());
+
+				property.SetValue(provider, objValue);
+				continue;
+			}
+			catch
+			{
+				// Unable to convert basic value
+			}
+
+			try
+			{
+				var value = JsonConvert.DeserializeObject(propertyValue.Value.ToString(), property.PropertyType);
+				property.SetValue(provider, value);
+			}
+			catch
+			{
+				// Unable to convert complex value
+			}
+		}
 	}
 
 	public abstract Task<AMProviderResult> ExecuteOrder(ADBot bot, AMOrderRequest order);
