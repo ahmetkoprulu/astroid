71da4cc68d9eac3e38eca5d279748fddb3dead3b
ahmetkoprulu <aensarkoprulu@gmail.com>
2023-07-14 11:01:22 +0300

    Added quantity property to webhook info

--- src/Astroid.Web/Client/src/components/Bots/WebhookInfo.vue
+++ src/Astroid.Web/Client/src/components/Bots/WebhookInfo.vue
@@ -1,22 +1,23 @@
 <template>
-  <div>
-    <b-form-group label="Webhook">
-      {{ `https://trade.ahmetkoprulu.com/api/bots/execute` }}
-    </b-form-group>
-    <b-form-group label="Open Orders">
-      <pre class="code">
+	<div>
+		<b-form-group label="Webhook">
+			{{ `https://trade.ahmetkoprulu.com/api/bots/execute` }}
+		</b-form-group>
+		<b-form-group label="Open Orders">
+			<pre class="code">
 
 { 
   "ticker": "BTCUSDT", 
   "leverage": 20, 
   "risk": "1 | 2 | ... | n", 
   "type": "open-long | open-short", 
+  "quantity": 100 (in unit of position size),
   "key": "{{ botKey }}" 
 }
       </pre>
-    </b-form-group>
-    <b-form-group label="Close Orders">
-      <pre class="code">
+		</b-form-group>
+		<b-form-group label="Close Orders">
+			<pre class="code">
 
 { 
   "ticker": "BTCUSDT", 
@@ -24,29 +25,29 @@
   "key": "{{ botKey }}" 
 }
       </pre>
-    </b-form-group>
-  </div>
+		</b-form-group>
+	</div>
 </template>
 
 <script>
 export default {
-  props: {
-    botKey: {
-      type: String,
-    },
-  },
+	props: {
+		botKey: {
+			type: String,
+		},
+	},
 };
 </script>
 
 <style>
 .code {
-  width: 406px;
-  background-color: #f5f5f5;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  padding-top: 6px;
-  padding-left: 12px;
-  padding-right: 12px;
-  padding-bottom: 6px;
+	width: 406px;
+	background-color: #f5f5f5;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+	padding-top: 6px;
+	padding-left: 12px;
+	padding-right: 12px;
+	padding-bottom: 6px;
 }
 </style>
